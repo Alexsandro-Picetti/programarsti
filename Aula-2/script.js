503aa6c4ddd68a92408b8f094d865b01226db8d0
Alexsandro-Picetti <alex.picetti@hotmail.com>
2024-10-08 20:38:52 -0300

    Aula-2

--- Aula-2/script.js
+++ Aula-2/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-    $.getJSON("./data.json", function(data) {
-        alert(data.name);
+$(document).ready(function () {
+    $("#button1").on("click", function (event) {
+        $.getJSON("./data.json", function (data) {
+            $("#resultado1").append(data.name + ", ");
+            $("#resultado1").append(data.age + " anos ");
+
+            for(var i in data.cars){
+                $("#resultado1").append(data.cars[i]);
+            }
+            $("#resultado1").append("<br>");
+        });
     });
+
+    $("#Limpar").on("click", function (event) {
+        $("div[id^=resultado]").empty();
+    });
+
 });
